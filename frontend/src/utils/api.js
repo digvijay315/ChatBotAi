c108d101d724c5ed6e2b7fdd0dce9f149fbf1430
digvijay315 <digvijaykumar.315@gmail.com>
2026-05-26 12:14:19 +0530

    update

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api',
-  baseURL:'https://chatbotai-gtpm.onrender.com',
+  baseURL:'https://chatbotai-gtpm.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
